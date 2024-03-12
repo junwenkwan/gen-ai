21eec58b85c905bffd68e1f307ee551408316b9e
junwenkwan <kwanjunwen@gmail.com>
2024-03-12 23:11:02 +1100

    One shot inference

--- instruction_prompt.py
+++ instruction_prompt.py
@@ -37,6 +37,7 @@ for i, index in enumerate(example_indices):
     print(dash_line)
 
 # Zero Shot Inference with the Prompt Template from FLAN-T5
+print("Zero Shot Inference")
 for i, index in enumerate(example_indices):
     dialogue = dataset['test'][index]['dialogue']
     summary = dataset['test'][index]['summary']
@@ -63,3 +64,54 @@ What was going on?
     print(f'MODEL GENERATION - ZERO SHOT:\n{output}\n')
     print(dash_line)
     
+# One Shot Inference
+def make_prompt(example_indices_full, example_index_to_summarize):
+    prompt = ''
+    for index in example_indices_full:
+        dialogue = dataset['test'][index]['dialogue']
+        summary = dataset['test'][index]['summary']
+        
+        # The stop sequence '{summary}\n\n\n' is important for FLAN-T5. Other models may have their own preferred stop sequence.
+        prompt += f"""
+Dialogue:
+
+{dialogue}
+
+What was going on?
+{summary}
+
+
+"""
+    
+    dialogue = dataset['test'][example_index_to_summarize]['dialogue']
+    
+    prompt += f"""
+Dialogue:
+
+{dialogue}
+
+What was going on?
+"""
+        
+    return prompt
+
+example_indices_full = [40]
+example_index_to_summarize = 200
+
+one_shot_prompt = make_prompt(example_indices_full, example_index_to_summarize)
+
+summary = dataset['test'][example_index_to_summarize]['summary']
+
+inputs = tokenizer(one_shot_prompt, return_tensors='pt')
+output = tokenizer.decode(
+    model.generate(
+        inputs["input_ids"],
+        max_new_tokens=50,
+    )[0], 
+    skip_special_tokens=True
+)
+
+print("One Shot Inference")
+print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
+print(dash_line)
+print(f'MODEL GENERATION - ONE SHOT:\n{output}')
