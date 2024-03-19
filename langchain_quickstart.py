9be70e1d406748e281a6f099d5b443bf872a1214
junwenkwan <kwanjunwen@gmail.com>
2024-03-19 14:30:43 +1100

    Add retrieval chain

--- langchain_quickstart.py
+++ langchain_quickstart.py
@@ -1,20 +1,50 @@
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 
 llm = Ollama(model="llama2")
 
-# Running llama2
+# Part 1: Running llama2
 # print(llm.invoke("how can langsmith help with testing?"))
 
-# Guiding response with a prompt template
+# Part 2: LLM Chain
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are world class technical documentation writer."),
     ("user", "{input}")
 ])
 
 output_parser = StrOutputParser()
-
 chain = prompt | llm | output_parser
-
+print("=== LLM Chain ===")
 print(chain.invoke({"input": "how can langsmith help with testing?"}))
+
+# Part 3: Retrieval Chain
+loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
+docs = loader.load()
+embeddings = OllamaEmbeddings()
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+retriever = vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+response = retrieval_chain.invoke({"input": "how can langsmith help with testing?"})
+print("=== Retrieval Chain ===")
+print(response["answer"])
