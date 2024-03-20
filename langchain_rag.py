aa4d8ab3250eee2fc1936d3983ccd250a29e9d80
junwenkwan <kwanjunwen@gmail.com>
2024-03-20 22:49:58 +1100

    Add conversational retrieval chain

--- langchain_rag.py
+++ langchain_rag.py
@@ -6,7 +6,12 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings import OllamaEmbeddings
+from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
+from langchain_core.prompts import format_document
+from langchain_core.runnables import RunnableParallel
+from langchain.prompts.prompt import PromptTemplate
 
+# Retrieval-augmented generation
 vectorstore = FAISS.from_texts(
     ["harrison worked at kensho"], embedding=OllamaEmbeddings()
 )
@@ -37,3 +42,55 @@ chain = (
 response = chain.invoke({"question": "where did harrison work", "language": "italian"})
 
 print(response)
+
+# Conversational Retrieval Chain
+_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+
+ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
+
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+
+def _combine_documents(
+    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
+):
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
+
+_inputs = RunnableParallel(
+    standalone_question=RunnablePassthrough.assign(
+        chat_history=lambda x: get_buffer_string(x["chat_history"])
+    )
+    | CONDENSE_QUESTION_PROMPT
+    | ChatOllama(temperature=0)
+    | StrOutputParser(),
+)
+_context = {
+    "context": itemgetter("standalone_question") | retriever | _combine_documents,
+    "question": lambda x: x["standalone_question"],
+}
+conversational_qa_chain = _inputs | _context | ANSWER_PROMPT | ChatOllama()
+
+response = conversational_qa_chain.invoke(
+    {
+        "question": "where did he work?",
+        "chat_history": [
+            HumanMessage(content="Who wrote this notebook?"),
+            AIMessage(content="Harrison"),
+        ],
+    }
+)
+
+print(response.content)
