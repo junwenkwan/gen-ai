0064c99e59bbd6df7e00364e2197700abd88a90b
junwenkwan <kwanjunwen@gmail.com>
2024-03-27 19:10:18 +1100

    Add a custom tool to lang graph

--- langchain_graph.py
+++ langchain_graph.py
@@ -1,21 +1,69 @@
 from dotenv import load_dotenv
+import json
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 from langgraph.graph import END, MessageGraph
+from langchain_core.messages import ToolMessage
+from langchain_core.tools import tool
+from langchain_core.utils.function_calling import convert_to_openai_tool
 
 load_dotenv()
 
+@tool
+def multiply(first_number: int, second_number: int):
+    """Multiplies two numbers together."""
+    return first_number * second_number
+
 model = ChatOpenAI(temperature=0)
+model_with_tools = model.bind(tools=[convert_to_openai_tool(multiply)])
 
 graph = MessageGraph()
 
-graph.add_node("oracle", model)
-graph.add_edge("oracle", END)
+def invoke_model(state):
+    return model_with_tools.invoke(state)
+
+graph.add_node("oracle", invoke_model)
+
+def invoke_tool(state):
+    tool_calls = state[-1].additional_kwargs.get("tool_calls", [])
+    multiply_call = None
+
+    for tool_call in tool_calls:
+        if tool_call.get("function").get("name") == "multiply":
+            multiply_call = tool_call
+
+    if multiply_call is None:
+        raise Exception("No adder input found.")
+
+    res = multiply.invoke(
+        json.loads(multiply_call.get("function").get("arguments"))
+    )
+
+    return ToolMessage(
+        tool_call_id=multiply_call.get("id"),
+        content=res
+    )
+
+graph.add_node("multiply", invoke_tool)
+
+graph.add_edge("multiply", END)
 
 graph.set_entry_point("oracle")
 
+def router(state):
+    tool_calls = state[-1].additional_kwargs.get("tool_calls", [])
+    if len(tool_calls):
+        return "multiply"
+    else:
+        return "end"
+
+graph.add_conditional_edges("oracle", router, {
+    "multiply": "multiply",
+    "end": END,
+})
+
 runnable = graph.compile()
 
-response = runnable.invoke(HumanMessage("What is 1 + 1?"))
+response = runnable.invoke(HumanMessage("What is 123 * 456?"))
 
 print(response)
