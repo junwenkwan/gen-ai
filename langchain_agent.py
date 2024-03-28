b6a9abdc79d21343521647296a5d058bd63bce71
junwenkwan <kwanjunwen@gmail.com>
2024-03-28 16:35:28 +1100

    Replace duckduckgo with google serper

--- langchain_agent.py
+++ langchain_agent.py
@@ -1,12 +1,11 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain import hub
-from langchain.agents import create_openai_tools_agent
-from langchain_community.tools import DuckDuckGoSearchResults
-from langchain.agents import AgentExecutor
+from langchain.agents import initialize_agent, Tool
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_community.utilities import GoogleSerperAPIWrapper
 
 load_dotenv()
 
@@ -14,18 +13,30 @@ llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
 
 prompt = hub.pull("hwchase17/openai-tools-agent")
 
-search = DuckDuckGoSearchResults()
+search = GoogleSerperAPIWrapper()
+
+search_tool = Tool(
+                    name="Google Serper",
+                    func=search.run,
+                    description="useful for when you need to ask with search"
+                )
 
 wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
-tools = [search, wikipedia]
+wikipedia_tool = Tool(
+                    name="Wikipedia",
+                    func=wikipedia.run,
+                    description="wikipedia"
+                )
 
-agent = create_openai_tools_agent(llm, tools, prompt)
+tools = [search_tool, wikipedia_tool]
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+agent = initialize_agent(tools, llm, verbose=True)
 
-agent_executor.invoke(
+results = agent.invoke(
     {
-        "input": "What is the relationship between Applied EV and Suzuki?",
+        "input": "What is the relationship between Applied EV and Suzuki? Please elaborate.",
     }
 )
+
+print(results['output'])
